dfd2d143a8c9760c64eefc6764bb60a179f27b7d
Lane Henslee <huncho@hygo.ai>
2024-05-31 05:49:15 -0500

    made admin stuff and external rewardTokens

--- contracts/RootAISecToken.sol
+++ contracts/RootAISecToken.sol
@@ -6,17 +6,34 @@ import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract RootAISecToken is ERC20, ReentrancyGuard {
     uint256 public constant REWARD_AMOUNT = 10 * 10**18; // 10 tokens
+    address creator;
+    mapping(address => bool) admins;
 
     event TokensRewarded(address indexed user, uint256 amount);
     event GuideUploaded(address indexed researcher, string guideHash);
     event AuditRequested(address indexed user, string contractHash);
 
-    constructor(uint256 initialSupply) ERC20("RootAISecToken", "RAS") {
-        _mint(msg.sender, initialSupply * 10**18); // Mint initial supply of tokens to contract deployer
+    modifier onlyCreator() {
+        require(msg.sender == creator, "Only creator can call this function");
+        _;
     }
 
-    function mint(address to, uint256 amount) external {
+    modifier onlyAdmin() {
+        require(admins[msg.sender], "Only admins can call this function");
+        _;
+    }
+
+    constructor(uint256 initialSupply) ERC20("RootAISecToken", "RAS") {
+        _mint(msg.sender, initialSupply * 10**18); // Mint initial supply of tokens to contract deployer
+        creator = msg.sender;
+    }
+
+    function mint(address to, uint256 amount) external onlyAdmin {
         _mint(to, amount);
+    }
+
+    function addAdmin(address admin) external onlyCreator {
+        admins[admin] = true;
     }
 
     function uploadGuide(string memory guideHash) external nonReentrant {
@@ -30,7 +47,7 @@ contract RootAISecToken is ERC20, ReentrancyGuard {
         emit AuditRequested(msg.sender, contractHash);
     }
 
-    function rewardTokens(address recipient, uint256 amount) internal {
+    function rewardTokens(address recipient, uint256 amount) external nonReentrant onlyAdmin {
         require(balanceOf(address(this)) >= amount, "Insufficient balance in the contract to reward");
         _transfer(address(this), recipient, amount);
         emit TokensRewarded(recipient, amount);
